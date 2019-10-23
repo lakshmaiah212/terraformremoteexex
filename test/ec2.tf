58d3c1d7120d9024ca22e5047f7ae7894a9a07b0
Your Name <you@example.com>
2019-10-23 14:07:14 +0530

    SERVER CHANGED

--- test/ec2.tf
+++ test/ec2.tf
@@ -1,9 +1,13 @@
+resource "aws_key_pair" "ec2key" {
+  key_name = "publicKey"
+  public_key = "${file("/home/${var.SSHUSR}/.ssh/id_rsa.pub")}"
+}
 resource "aws_instance" "sonar" {
   ami           = "ami-0b69ea66ff7391e80"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0b4e3b9d8afb00cb4"]
   subnet_id = "subnet-0292e63a287192212"
-  key_name = "endpoint"
+  key_name = "${aws_key_pair.ec2key.key_name}"
    provisioner "remote-exec" {
     
        connection {
@@ -12,19 +16,18 @@ resource "aws_instance" "sonar" {
             host     = "${self.public_ip}"
             port = 22
             password= "${var.SSHPASS}"
-        
-          
+            private_key = "${file("home/${var.SSHUSR}/.ssh/id_rsa")}"
+            
             
           }
 
     
     inline = [
-      "sudo yum install ansible git -y",
+      "sudo yum install git -y",
     ]
   }
 
   tags = {
-    Name = "studentapp"
-    project_env = "TEST"
+    Name = "sonar"
   }
 }
