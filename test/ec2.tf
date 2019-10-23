a0fd6ae28f1d24a7b490f62cfaf30a7735afae76
Your Name <you@example.com>
2019-10-23 17:54:34 +0530

    server

--- test/ec2.tf
+++ test/ec2.tf
@@ -1,13 +1,10 @@
-resource "aws_key_pair" "ec2key" {
-  key_name = "publicKey"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
-}
+
 resource "aws_instance" "sonar" {
   ami           = "ami-0b69ea66ff7391e80"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0b4e3b9d8afb00cb4"]
   subnet_id = "subnet-0292e63a287192212"
-  key_name = "${aws_key_pair.ec2key.key_name}"
+  key_name = "endpoint"
    provisioner "remote-exec" {
     
        connection {
@@ -16,7 +13,7 @@ resource "aws_instance" "sonar" {
             host     = "${self.public_ip}"
             port = 22
             password= "devops321"
-            private_key = "${file("~/.ssh/id_rsa")}"
+            private_key = "${file("/var/lib/jenkins/endpoint.ppk")}"
             
             
           }
