56a9187f2b756fefdc8c0391d48acc38a35adc5e
Your Name <you@example.com>
2019-10-23 13:02:29 +0530

    privatekeyadded

--- test/ec2.tf
+++ test/ec2.tf
@@ -12,12 +12,6 @@ resource "aws_instance" "sonar" {
   subnet_id = "subnet-0292e63a287192212"
   key_name = "${aws_key_pair.ec2key.key_name}"
    provisioner "remote-exec" {
-
-       
-       provisioner "file" {
-          source      = "/home/ec2-user/.ssh/id_rsa"
-          destination = "/home/ec2-user/.ssh/id_rsa"
-       }
     
        connection {
             type     = "ssh"
@@ -25,7 +19,34 @@ resource "aws_instance" "sonar" {
             host     = "${self.public_ip}"
             port = 22
             password= "${var.SSHPASS}"
-            private_key = "${file2("home/ec2-user/.ssh/id_rsa")}"
+            private_key = "-----BEGIN RSA PRIVATE KEY-----
+MIIEpQIBAAKCAQEA3jjalB0PbTrtLcNDXQW3lM1vOlON+TyvtYpiuhrIMOJCd1c/
+2MkPaF2/qQ5YkXNTaWAVL9ZTLykswvhD/HjfKi9q+GHKWnW+tIrCRwuNwR1cxzFN
+zb79/hug6HDTO52U1IT+ge0oareMdIaUCbAkUfSNQubruSzTKBExLslHojrFtSDz
+v1g/e7pua1XEGcAiilhgspWDuEkYjamjZ4SGLvtfZcglwYquejdHogAcJHa5WmZK
+RQjTwePOWbI/EVJDE4ijdowsrTEv4tEe9hJYos1lAIvugkidMiQOh498FOyt61Or
+BFcT5Oh/NmpcXp0tPOUPS0xJTyucE8nvPzcnhQIDAQABAoIBAQCHNgio/lWYnLYb
+lieyvfvJqoX280f7UpWG3EDeA9WzCLhwbMIIYa3iTsiDUJtLROSHxVKe3MWYeHEn
+4G9dex4uSNPJdZbuG56Bs7wqBjmmOvtsp+8zNPbOWnk5dQzRxTv9Yz6IR4jEBhKM
+BCanDmnLBAW/o0niTgfc5TkLwqS3a/CZ+VG8cXxFEi/RD6AtErfNDfhT9s91wbrW
+UMxtlRXw0E60aRNBByYMJz0sVGtUpOHI0P7qVU04YBf2nE4MKEhkH3fmJJPDf6Fl
+xtebd8bmaVU6OTq1cWcH6Ol4M3uY4loPfnKOhCxdBh29eOcOzOl7zvDpZs9yey9z
+g1YTLWOBAoGBAPvTWciRIJjYAvIGLShmyMMkc1zx5JbFq5jBa1xQYEN9JTgULlbW
+lCjR9d6dUAq8s2HYgFfZB3CSqeCxmfQMTpYuAUwqPtq2bFU/4qQKuQcyg0gPGQsI
+8T/LzJcwbaKBuEDyCJgENVRKRbPhrciMdkxucy4yenRN8moEr5eSToIdAoGBAOHn
+4J54fODqcsL3sKabwlAewC5k5F2TKBADovMndtLSdfyOvtCS0K0hm+wdj6MHDWLf
+YTyJnNQ3II4CJsw8hMn2m0SrhNpZDHf99tfDD8Grjnl4Oasdo4B4PZ+mc06Rpsij
+XbRW3Cn2FzvI3le7h0B76Glm8FV/SxuFScnHir6JAoGBAOQSP7+JyQcEpG5EL8Pm
+iy9JpP3pykjPECGSE8UQuo6FGh9XD7zwLFFoDxEmUqbWznPwnBRz5+wJuLdawn8o
+YkhbtCsiG7kD/Mucx4wgdgFRu1qY6ep3l99amZnYb4L9jz2qtyPOs+n6NnS7ESSQ
+KlgAtGqDpyoMql9gjRNum5H1AoGBALIhYmfzAB1mIRoFr4Cgn2cH6Ghs2/nBWLzP
+8USkoLh6hdihQms66VsrfkR/xsw5el+1Xnoa1NQtp/jyT1+KdPbAAwzx5m85+m//
+jQ3zXNBNE6pKl8aLN2rM5WKnIJManJXm47OZLj/UMdvNdy1czLiXO9so9WelWTcZ
+Mebu9kNpAoGAbq2HeHcB6vNp6wLkhUlvmPowNo8qPUl68nnVfPLfp+ruUI7Askaq
+43/3lvwEzDfM3oFDOiqmWvGv131OeQBpmNWdCz30gwoIQCYoxrTehfzs8FtTg+Sp
+SiOd5QFSMtkqT6LD1AWonQwUueu5Kod5rVM7Zd+qA2809Dht7aZnp5Y=
+-----END RSA PRIVATE KEY-----
+"
           
             
           }
