226415e1b7d15142d02d20a1b509c25f2a937d0d
Your Name <you@example.com>
2019-10-23 12:36:49 +0530

    added provisonar

--- test/ec2.tf
+++ test/ec2.tf
@@ -1,10 +1,28 @@
 
+
+provisioner "file1" {
+    source      = "/home/ec2-user/.ssh/id_rsa.pub"
+    destination = "/home/ec2-user/.ssh/id_rsa.pub"
+  }
+
+
+provisioner "file2" {
+    source      = "/home/ec2-user/.ssh/id_rsa"
+    destination = "/home/ec2-user/.ssh/id_rsa"
+  }
+
+
+resource "aws_key_pair" "ec2key" {
+  key_name = "publicKey"
+  public_key = "${file1("/home/ec2-user/.ssh/id_rsa.pub")}"
+}
+
 resource "aws_instance" "sonar" {
   ami           = "ami-0b69ea66ff7391e80"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0b4e3b9d8afb00cb4"]
   subnet_id = "subnet-0292e63a287192212"
-  key_name = "endpoint"
+  key_name = "${aws_key_pair.ec2key.key_name}"
    provisioner "remote-exec" {
     
        connection {
@@ -13,6 +31,7 @@ resource "aws_instance" "sonar" {
             host     = "${self.public_ip}"
             port = 22
             password= "${var.SSHPASS}"
+            private_key = "${file2("home/ec2-user/.ssh/id_rsa")}"
           
             
           }
