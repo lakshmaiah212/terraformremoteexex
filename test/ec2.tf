f4eb9a11380da90f814ad2f6676f9fbd5bc7c3d1
Your Name <you@example.com>
2019-10-23 13:38:50 +0530

    serbver changes

--- test/ec2.tf
+++ test/ec2.tf
@@ -1,17 +1,9 @@
-
-
-resource "aws_key_pair" "ec2key" {
-  key_name = "publickey"
-  public_key = "${"/home/ec2-user/.ssh/id_rsa.pub"}"
-
-}
-
 resource "aws_instance" "sonar" {
   ami           = "ami-0b69ea66ff7391e80"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0b4e3b9d8afb00cb4"]
   subnet_id = "subnet-0292e63a287192212"
-  key_name = "${aws_key_pair.ec2key.key_name}"
+  key_name = "${"/home/ec2-user/.ssh/id_rsa.pub"}"
    provisioner "remote-exec" {
     
        connection {
