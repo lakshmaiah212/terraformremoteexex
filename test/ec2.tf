89718b40f72604ed37e6d0aa733a9154a316fa02
Your Name <you@example.com>
2019-10-23 12:25:16 +0530

    changesinserver

--- test/ec2.tf
+++ test/ec2.tf
@@ -1,15 +1,10 @@
 
-
-resource "aws_key_pair" "ec2key" {
-  key_name = "publicKey"
-  public_key = "${file("/home/ec2-user/.ssh/id_rsa.pub")}"
-}
 resource "aws_instance" "sonar" {
   ami           = "ami-0b69ea66ff7391e80"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0b4e3b9d8afb00cb4"]
   subnet_id = "subnet-0292e63a287192212"
-  key_name = "${aws_key_pair.ec2key.key_name}"
+  key_name = "endpoint"
    provisioner "remote-exec" {
     
        connection {
@@ -18,8 +13,7 @@ resource "aws_instance" "sonar" {
             host     = "${self.public_ip}"
             port = 22
             password= "${var.SSHPASS}"
-            private_key = "${file("home/ec2-user/.ssh/id_rsa")}"
-            
+          
             
           }
 
